df24f1e86beab20cd2860dfd6842ad107b737578
Alessandro Ferdian <alessandroferdian128@gmail.com>
2020-12-02 17:17:33 +0700

    obscure console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router, NavigationEnd } from '@angular/router';
 
 import localeId from '@angular/common/locales/id'; 
 import { registerLocaleData } from '@angular/common';
+import { environment } from 'src/environments/environment';
 registerLocaleData(localeId, 'id'); 
 
 
@@ -17,6 +18,11 @@ export class AppComponent {
   onNavigate: boolean = false
 
   constructor(private router: Router){
+    if(environment.production){
+      console.warn("Console log will be obscured!")
+      window.console.log = function () {};
+    }
+    
     this.router.events.subscribe(
       (res)=>{
         if(res instanceof NavigationEnd){
